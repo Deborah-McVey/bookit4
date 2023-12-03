20a103e436c851de130d06d5bb76212f2a8c71af
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-12-03 18:09:25 -0500

    beginning again, looks good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { LibraryComponent } from './library/library.component';
+import { BookListComponent } from './bookshelf/book-list/book-list.component';
+import { BookDetailsComponent } from './bookshelf/book-details/book-details.component';
+import { BookResultsComponent } from './library/book-results/book-results.component';
+import { BookSearchComponent } from './library/book-search/book-search.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { BookComponent } from './shared/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookshelfComponent,
+    LibraryComponent,
+    BookListComponent,
+    BookDetailsComponent,
+    BookResultsComponent,
+    BookSearchComponent,
+    NavigationComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
